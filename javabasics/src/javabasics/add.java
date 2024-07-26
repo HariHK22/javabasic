15449b602c3b547de6eb8e0d9a8767410e128c97
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-26 16:10:35 +0530

    sum and string

--- javabasics/src/javabasics/add.java
+++ javabasics/src/javabasics/add.java
@@ -1,10 +1,20 @@
 package javabasics;
+import java.util.Scanner;
 
 public class add {
 
 	public static void main (String[] args) {
-		int a=1;
-		
+		Scanner scan= new Scanner(System.in);
+		System.out.print("Enter the number 1 :");
+		int a= scan.nextInt();
+		System.out.print("Enter the number 2 : ");
+		int b= scan.nextInt();
+		int c=a+b;
+		scan.nextLine();
+		System.out.println("Enter your name : ");
+		String name = scan.nextLine();
+		System.out.println("And your total values : "+c);
+		System.out.println("And your name is : "+name);
 		
 	}
 
