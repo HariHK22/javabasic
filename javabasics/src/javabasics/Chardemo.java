296eea1963e5bf27991419269cda2e1acd51051d
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-28 19:17:35 +0530

    java char

---
+++ javabasics/src/javabasics/Chardemo.java
@@ -0,0 +1,24 @@
+package javabasics;
+
+public class Chardemo {
+	public static void main(String[] args)
+	{
+		char ch = 'A';
+		System.out.println(ch);
+		
+		char ch1 = 65;
+		System.out.println(ch1);
+		
+		char ch2 = '\u0024';
+		System.out.println(ch2);
+		
+		char ch3 = '\u0025';
+		System.out.println(ch3);
+		
+		int a= 'C';
+		System.out.println(a);
+		
+		
+	}
+
+}
