d82d2dab03f17eff5d565147a986918fb2688308
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-28 22:33:26 +0530

    if else

---
+++ javabasics/src/Daytwo/IfElse.java
@@ -0,0 +1,19 @@
+package Daytwo;
+
+public class IfElse {
+
+	public static void main(String[] args)
+	{
+		short age =17;
+		if(age>=18)
+		{
+			System.out.println("Eligible to vote");
+		}
+		else
+		{
+			System.out.println("You are not eligible to vote");
+		}
+		System.out.println("If else example");
+	}
+
+}
