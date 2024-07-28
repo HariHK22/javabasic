7c753124e29ad064788848afd909359671266e5d
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-28 22:27:10 +0530

    If condition

---
+++ javabasics/src/Daytwo/IfCondition.java
@@ -0,0 +1,13 @@
+package Daytwo;
+
+public class IfCondition {
+
+	public static void main(String[] args)
+	{
+		short a=5;
+		if(a!= 5)
+			System.out.println("it's not an 5");
+		System.out.println("the entred number was an 5");
+	}
+
+}
