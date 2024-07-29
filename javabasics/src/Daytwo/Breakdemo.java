03ce1f0091ca3d6c38f1641b95d1623cbd6f4f2d
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-29 14:44:48 +0530

    Break demo

---
+++ javabasics/src/Daytwo/Breakdemo.java
@@ -0,0 +1,16 @@
+package Daytwo;
+
+public class Breakdemo {
+
+	public static void main(String[] args)
+	{
+		for(int i=0;i<10;i++)
+		{
+			if(i==5)
+			{
+				break;
+			}
+			System.out.println(i);
+		}
+	}
+}
