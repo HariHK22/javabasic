0191ff5e53eba25981060e24aaec13335d0f1a7f
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-29 09:33:52 +0530

    For loop demo

---
+++ javabasics/src/Daytwo/ForLoopDemo.java
@@ -0,0 +1,14 @@
+package Daytwo;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("It was an I value : "+i);
+		}
+	}
+	
+
+}
