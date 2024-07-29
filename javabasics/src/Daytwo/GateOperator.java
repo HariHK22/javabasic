1fac8c83fa4a8dae839cc15db39890adf1db2604
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-29 18:45:37 +0530

    Gate and operator

---
+++ javabasics/src/Daytwo/GateOperator.java
@@ -0,0 +1,27 @@
+package Daytwo;
+
+public class GateOperator {
+	public static void main(String[] args)
+	{
+		byte x=10,y=11;
+		byte a=5,b=7;
+		if(a>b)
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+		if(!(x<y) || (x==y))
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+		
+	}
+
+}
