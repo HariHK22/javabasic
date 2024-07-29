e3e71ead777c8a392b3be74f7822989f19dddccf
HariHK22 <120644948+HariHK22@users.noreply.github.com>
2024-07-29 10:20:17 +0530

    For Each Loop

---
+++ javabasics/src/Daytwo/ForEachloop.java
@@ -0,0 +1,25 @@
+package Daytwo;
+
+public class ForEachloop {
+
+	public static void main(String[] args) {
+		int[] a= {15,16,17,18,19};
+		for(int i:a)
+		{
+			System.out.println(i);
+		}
+		
+		char[] b= {'j','a','v','a'};
+		for (char j:b)
+		{
+			System.out.print(j);
+		}
+		System.out.println(" ");
+		String[] c= {"java","programming","Language"};
+		for(String k:c)
+		{
+			System.out.print(k+" ");
+		}
+	}
+
+}
